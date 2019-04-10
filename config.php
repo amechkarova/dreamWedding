88abb720f52920a3b7d2da246832b7c9b420b914
anelsa <aneliyaivanova96@gmail.com>
2019-04-10 14:05:28 +0300

    First commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	require 'create_db.php';
+	require 'create_users_table.php';
+	require 'create_guestcategories_table.php';
+	require 'fill_guestcategories_table.php';
+	require 'create_tables.php';
+	require 'fill_hairdressingsalons_table.php';
+	require 'add_restaurantsIndb.php';
+?>
